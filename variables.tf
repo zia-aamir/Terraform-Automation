b8d7bcca0bf5bf16204a841c4fd73f4d10de69da
Shah Shanediwan <aamirshanediwan@gmail.com>
2026-04-15 23:36:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "key_name" {
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t3.small" 
 }
 
 variable "security_group" { 
