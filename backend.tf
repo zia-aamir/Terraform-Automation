3cd83edaac53b3f77ca8701899810183a09b8ac4
Shah Shanediwan <aamirshanediwan@gmail.com>
2026-04-15 23:35:04 +0530

    Change S3 backend region from us-east-1 to ap-south-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "zia-project-terraform-state-123"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
